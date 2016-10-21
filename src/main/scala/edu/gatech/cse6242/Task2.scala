1a16f8030a13b323a2158513b4b2f4759e50e099
Matvei Nazaruk <matvei.nazaruk@gmail.com>
2016-10-21 21:22:35 +0300

    Added description, refactored task.

--- src/main/scala/edu/gatech/cse6242/Task2.scala
+++ src/main/scala/edu/gatech/cse6242/Task2.scala
@@ -7,23 +7,18 @@ import org.apache.spark.SparkConf
 object Task2 {
   def main(args: Array[String]) {
 
-    System.out.println("******************************")
-    // return;
     val sc = new SparkContext(new SparkConf().setAppName("Task2"))
 
     // read the file
     val file = sc.textFile("hdfs://quickstart.cloudera:8020/" + args(0))
 
-    /* TODO: Needs to be implemented */
- 
+    //split and get 2d and 3d elements
     val splitted = file.map(line => (line.split("\t")(1), line.split("\t")(2).toInt))
  
-    // count the occurrence of each word
+    // sum edges
     val graphCounts = splitted.reduceByKey(_ + _)
   	
     // store output on given HDFS path.
-    // YOU NEED TO CHANGE THIS
     graphCounts.map(c => c._1 + "\t" + c._2).saveAsTextFile("hdfs://localhost:8020" + args(1))
-    // System.out.println(graphCounts.collect().mkString(", "))
   }
 }
